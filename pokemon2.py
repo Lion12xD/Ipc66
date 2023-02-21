66490e65a814de5bbe93b042da5b360e4d6a740f
Lion12xD <103289468+Lion12xD@users.noreply.github.com>
2023-02-21 16:58:38 -0600

    agregamos mas detalles al pokemon2.py

--- pokemon2.py
+++ pokemon2.py
@@ -1,3 +1,8 @@
+# Script en python que consulta el api de pokemon 
+# para listar los nombres de pokemon pero se le agrego 
+# interacción para que listaras más pokemons segun se vaya requiriendo. 
+# Contribuyo: Leonardo Hasiel Alvarado Zamora 
+# Fecha: 21/02/2023 
 import requests
 # Nombre: Leonardo Hasiel Alvarado Zamora
 # Matricula: 1949295
